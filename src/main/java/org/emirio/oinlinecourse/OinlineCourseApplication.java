03cd3d973095d35b7a365fd7ad5ff7ab3dbf5d4e
EmirioBomb <wjhlfamily@gmail.com>
2020-09-03 10:47:46 +0800

    initialize

---
+++ src/main/java/org/emirio/oinlinecourse/OinlineCourseApplication.java
@@ -0,0 +1,13 @@
+package org.emirio.oinlinecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OinlineCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OinlineCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/emirio/oinlinecourse/OinlineCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emirio.oinlinecourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OinlineCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
